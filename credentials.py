254e8e4f0676bcd6158475070f2eb0bbee93f5a8
Aleksey Bliznichenko <ravikgames1111@gmail.com>
2024-12-24 13:37:41 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -3,5 +3,5 @@
 # This file will be pushed in the last commit
 # After that it will be private again and previous api key will be blocked to prevent other people form using it
 
-api_key = 'WO2bAzLKX2c2jbGc69CA5CJx3yfQEJy3'
+api_key = 'vZAtJ7ZXKFmU9xbHUAuqakkDhd5aFVLf'
 # Also you probably need to set your own key here, because limit get exceeded very fast
