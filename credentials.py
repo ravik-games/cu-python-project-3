662656180b47fa98aa03c43f31a41f2951feefef
ravik <ravikgames1111@gmail.com>
2024-12-24 12:22:50 +0300

    Last commit (API key)

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# Separate file to hide API keys during development and testing
+# CU should think about some method to allow students hide their credentials when uploading a homework
+# This file will be pushed in the last commit
+# After that it will be private again and previous api key will be blocked to prevent other people form using it
+
+api_key = 'WO2bAzLKX2c2jbGc69CA5CJx3yfQEJy3'
+# Also you probably need to set your own key here, because limit get exceeded very fast
